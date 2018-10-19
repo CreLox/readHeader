9603f46c59db44e376ad9f367c4e4a10242bbba2
Chu Chen <CreLox@users.noreply.github.com>
2018-10-19 10:59:24 -0400

    Add files via upload

--- readHeader.m
+++ readHeader.m
@@ -3,8 +3,10 @@ function FileHeaderStruct = readHeader(FormatExplanationTable, FilePath)
     %% Columns in FormatExplanationTable (each row is for one property):
     % This column stores the name of a property
     PropertyNameIdx      = 1;
-    % This column stores the starting byte (NOT bit) position of the
-    % property
+    % This column stores the (starting BYTE - 1) position of the property
+    % e.g. if a property of the "uint32" type spans 4 bytes, #17-#20, the
+    % (starting BYTE - 1) position of this property will be 16. If empty,
+    % the current position FID will be used.
     StartingByteIdx      = 2;
     % This column stores the data type, which determines the byte length of
     % the property
@@ -17,7 +19,8 @@ function FileHeaderStruct = readHeader(FormatExplanationTable, FilePath)
     % FormatExplanationTable can either be a MATLAB table class object or
     % the path to the file containing the table
     if ~istable(FormatExplanationTable)
-        FormatExplanationTable = readtable(FormatExplanationTable, 'Delimiter', '\t');
+        FormatExplanationTable = readtable(FormatExplanationTable, ...
+            'Delimiter', '\t', 'ReadVariableNames', false, 'TextType', 'string');
     end
     % if FilePath is not supplied, file seletion window will pop up to
     % allow user to select the file
@@ -30,16 +33,19 @@ function FileHeaderStruct = readHeader(FormatExplanationTable, FilePath)
     FID = fopen(FilePath);
     FileHeaderStruct = struct;
     for i = 1 : size(FormatExplanationTable, 1)
-        fseek(FID, FormatExplanationTable{i, StartingByteIdx}, 'bof');
-        if (size(FormatExplanationTable, 2) == 3) || ...
-                isempty(FormatExplanationTable{i, TranslationMethodIdx}{:})
-            % If TranslationMethod is not supplied, type coercion is the default
-            Translate = str2func(FormatExplanationTable{i, TypeIdx}{:});
-        else
-            Translate = str2func(FormatExplanationTable{i, TranslationMethodIdx}{:});
+        if isnumeric(FormatExplanationTable{i, StartingByteIdx}) && ...
+                ~isnan(FormatExplanationTable{i, StartingByteIdx})
+            fseek(FID, FormatExplanationTable{i, StartingByteIdx}, 'bof');
         end
-        FileHeaderStruct.(FormatExplanationTable{i, PropertyNameIdx}{:}) = ...
-            Translate(fread(FID, 1, FormatExplanationTable{i, TypeIdx}{:}));
+        if (size(FormatExplanationTable, 2) == 3) || ...
+                strcmp(FormatExplanationTable{i, TranslationMethodIdx}, "")
+            % If TranslationMethod is not supplied, type coercion is the default
+            Translate = str2func(FormatExplanationTable{i, TypeIdx});
+        else
+            Translate = str2func(FormatExplanationTable{i, TranslationMethodIdx});
+        end
+        FileHeaderStruct.(FormatExplanationTable{i, PropertyNameIdx}) = ...
+            Translate(fread(FID, 1, FormatExplanationTable{i, TypeIdx}));
     end
     fclose(FID);
 end
