3c5f591a9bb35da091a126c79c4630d34e2dfd12
Chu Chen <CreLox@users.noreply.github.com>
2018-10-19 14:33:03 -0400

    Update readHeader.m

--- readHeader.m
+++ readHeader.m
@@ -1,29 +1,30 @@
 % FileHeaderStruct = readHeader(FormatExplanationTable, FilePath(OPTIONAL))
 function FileHeaderStruct = readHeader(FormatExplanationTable, FilePath)
-    %% Columns in FormatExplanationTable (each row is for one property):
-    % This column stores the name of a property
+    %% Columns in FormatExplanationTable (each row is for one property)
+    % This column stores the name of a property.
     PropertyNameIdx      = 1;
-    % This column stores the (starting BYTE - 1) position of the property
-    % e.g. if a property of the "uint32" type spans 4 bytes, #17-#20, the
-    % (starting BYTE - 1) position of this property will be 16. If empty,
-    % the current position FID will be used.
+    % This column stores the (starting BYTE - 1) position of the property.
+    % (e.g. if a property of the "uint32" type spans 4 bytes, #17-#20, the
+    % (starting BYTE - 1) position of this property will be 16.)
+    % If the (starting BYTE - 1) position is not supplied/NaN, the current
+    % position ftell(FID) will be used.
     StartingByteIdx      = 2;
     % This column stores the data type, which determines the byte length of
-    % the property
+    % the property.
     TypeIdx              = 3;
-    % OPTIONAL: this column stores any simple data handling method
-    % utilizing a MATLAB function handle
+    % (OPTIONAL) This column stores any (simple) data handling method
+    % utilizing a MATLAB function handle.
     TranslationMethodIdx = 4;
     
-    %% Inputs processing
+    %% Robust inputs
     % FormatExplanationTable can either be a MATLAB table class object or
-    % the path to the file containing the table
+    % the path to the file containing the table.
     if ~istable(FormatExplanationTable)
         FormatExplanationTable = readtable(FormatExplanationTable, ...
             'Delimiter', '\t', 'ReadVariableNames', false, 'TextType', 'string');
     end
-    % if FilePath is not supplied, file seletion window will pop up to
-    % allow user to select the file
+    % if FilePath is not supplied, a file seletion window will pop up to
+    % allow user to select the file to be read.
     if ~exist('FilePath', 'var') || isempty(FilePath)
         [Filename, Path] = uigetfile('*.*');
         FilePath = strcat(Path, Filename);
@@ -39,7 +40,7 @@ function FileHeaderStruct = readHeader(FormatExplanationTable, FilePath)
         end
         if (size(FormatExplanationTable, 2) == 3) || ...
                 strcmp(FormatExplanationTable{i, TranslationMethodIdx}, "")
-            % If TranslationMethod is not supplied, type coercion is the default
+            % If TranslationMethod is not supplied, type coercion is the default.
             Translate = str2func(FormatExplanationTable{i, TypeIdx});
         else
             Translate = str2func(FormatExplanationTable{i, TranslationMethodIdx});
