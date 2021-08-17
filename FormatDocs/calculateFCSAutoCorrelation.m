c8e6edf75e93cee47691c9a6ffb5994041348b30
Chu Chen <CreLox@users.noreply.github.com>
2021-08-17 15:58:36 -0400

    Add files via upload

--- FormatDocs/calculateFCSAutoCorrelation.m
+++ FormatDocs/calculateFCSAutoCorrelation.m
@@ -1,6 +1,6 @@
-% [LagInSeconds, FCSAC, Filename] =
-% calculateFCSAutoCorrelation(SegmentLengthInSeconds, MinLagInSeconds,
-% MaxLagInSeconds, LagIntervalInSeconds)
+% [Trace, Filename] = calculateFCSAutoCorrelation(SegmentLengthInSeconds,
+% MinLagInSeconds, MaxLagInSeconds, LagIntervalInSeconds,
+% MicroTimeFilteringFunction)
 % (A file selection UI window will pop up to allow the user to select a
 % .fcs file)
 % Note: 1. 'LagIntervalInSeconds' is only needed when calculating the
@@ -18,19 +18,16 @@
 % of time points.
 % 4. Please put this .m file and AlbaV5FCSFileFormat.txt under the same
 % folder in MATLAB search paths.
-% 5. Currently this function is supposed to only handle .fcs files
-% containing a single trace.
-function [LagInSeconds, FCSAC, Filename] = calculateFCSAutoCorrelation(...
+function [Trace, Filename] = calculateFCSAutoCorrelation(...
     SegmentLengthInSeconds, MinLagInSeconds, MaxLagInSeconds, ...
-    LagIntervalInSeconds)
+    LagIntervalInSeconds, MicroTimeFilteringFunction)
     %% Parameter setting/read the header
     FCSHeader = readHeader(strcat(fileparts(mfilename('fullpath')), ...
         filesep, 'AlbaV5FCSFileFormat.txt'), [], '.fcs');
     [~, Filename, ~] = fileparts(FCSHeader.FilePath);
-    if (FCSHeader.ChannelNumber > 1) || ...
-        (FCSHeader.PositionSeriesCount > 1) || ...
+    if (FCSHeader.PositionSeriesCount > 1) || ...
         (FCSHeader.TimeSeriesCount > 1)
-        error('This .fcs file contains more than one trace!');
+        error('This .fcs file contains more than 1 position/time series!');
     end
     if (FCSHeader.AcquisitionMode == 0)
         % Count mode - LagIntervalInSeconds is always set to 1 /
@@ -47,50 +44,82 @@ function [LagInSeconds, FCSAC, Filename] = calculateFCSAutoCorrelation(...
     % instability is often observed at the beginning of data acquisition.
     StartFromThisSegment = 2;
     
-    %% Read the trace
-    Trace = struct();
+    %% Read the macro time trace(s)
+    Trace = cell(1, FCSHeader.ChannelNumber);
     fid = fopen(FCSHeader.FilePath, 'r');
-    fseek(fid, FCSHeader.MacroTimeOffset, 'bof'); 
-    Trace.Length = fread(fid, 1, 'uint64') / double(FCSHeader.BytesPerDatum);
-    if (FCSHeader.BytesPerDatum == 2)
-        Trace.Data = fread(fid, Trace.Length, 'uint16');
-    else
-        Trace.Data = fread(fid, Trace.Length, 'uint32');
+    fseek(fid, FCSHeader.MacroTimeOffset, 'bof');
+    for j = 1 : FCSHeader.ChannelNumber
+        Trace{j}.Length = fread(fid, 1, 'uint64') / ...
+            double(FCSHeader.BytesPerDatum);
+        if (FCSHeader.BytesPerDatum == 2)
+            Trace{j}.Data = fread(fid, Trace{j}.Length, 'uint16');
+        else
+            Trace{j}.Data = fread(fid, Trace{j}.Length, 'uint32');
+        end
+        if (FCSHeader.AcquisitionMode ~= 0) 
+            Trace{j}.ArrivalTime = cumsum(Trace{j}.Data) / FCSHeader.ClockFrequency;
+        end
+    end
+    
+    %% Read the micro time trace(s) & apply filter(s)
+    if (FCSHeader.AcquisitionMode == 2) && ...
+        exist('MicroTimeFilteringFunction', 'var') && ...
+        ~isempty(MicroTimeFilteringFunction)
+        fseek(fid, FCSHeader.MicroTimeOffset, 'bof');
+        for j = 1 : FCSHeader.ChannelNumber
+            fread(fid, 1, 'uint64');
+            if (FCSHeader.BytesPerDatum == 2)
+                Trace{j}.MicroTimeData = single(fread(fid, Trace{j}.Length, 'uint16')) * ...
+                    FCSHeader.MicroTimeRange / ...
+                    single(FCSHeader.MaxMicroTimeResolution);
+            else
+                Trace{j}.MicroTimeData = single(fread(fid, Trace{j}.Length, 'uint32')) * ...
+                    FCSHeader.MicroTimeRange / ...
+                    single(FCSHeader.MaxMicroTimeResolution);
+            end
+            Filter = MicroTimeFilteringFunction{j}(Trace{j}.MicroTimeData);
+            Trace{j}.Data = Trace{j}.Data(Filter);
+            Trace{j}.MicroTimeData = Trace{j}.MicroTimeData(Filter);
+            Trace{j}.ArrivalTime = Trace{j}.ArrivalTime(Filter);
+            Trace{j}.Length = length(Trace{j}.Data);
+        end
     end
     
     %% Calculate ACF and mean
-    SegmentsNumber = floor(FCSHeader.SampleTime / SegmentLengthInSeconds);
-    ACF = zeros(SegmentsNumber - StartFromThisSegment + 1, length(...
-        ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
-        floor(MaxLagInSeconds / LagIntervalInSeconds)));
-    if (FCSHeader.AcquisitionMode == 0)
-        % Count mode - Trace.Data represent detected photon counts within
-        % intervals of length 1 / FCSHeader.SampleFrequency
-        for i = StartFromThisSegment : SegmentsNumber
-            TimeSeries = Trace.Data((SegmentLengthInSeconds / LagIntervalInSeconds * (i - 1) + 1) : ...
-                (SegmentLengthInSeconds / LagIntervalInSeconds * i));            
-           [~, ACF(i - StartFromThisSegment + 1, :)] = ...
-               calculateDiscreteTimeSeriesStatistics(TimeSeries, ...
-               ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
-               floor(MaxLagInSeconds / LagIntervalInSeconds));
+    for j = 1 : FCSHeader.ChannelNumber
+        SegmentsNumber = floor(FCSHeader.SampleTime / SegmentLengthInSeconds);
+        ACF = zeros(SegmentsNumber - StartFromThisSegment + 1, length(...
+            ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
+            floor(MaxLagInSeconds / LagIntervalInSeconds)));
+        if (FCSHeader.AcquisitionMode == 0)
+            % Count mode - Trace.Data represent detected photon counts within
+            % intervals of length 1 / FCSHeader.SampleFrequency
+            for i = StartFromThisSegment : SegmentsNumber
+                TimeSeries = Trace{j}.Data((SegmentLengthInSeconds / LagIntervalInSeconds * (i - 1) + 1) : ...
+                    (SegmentLengthInSeconds / LagIntervalInSeconds * i));            
+               [~, ACF(i - StartFromThisSegment + 1, :)] = ...
+                   calculateDiscreteTimeSeriesStatistics(TimeSeries, ...
+                   ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
+                   floor(MaxLagInSeconds / LagIntervalInSeconds));
+            end
+        else
+            % Time-tagged (time-resolved) mode - Trace.Data represent macro
+            % times of detected photons            
+            for i = StartFromThisSegment : SegmentsNumber
+                TimeSeries = histcounts(Trace{j}.ArrivalTime, ...
+                    SegmentLengthInSeconds * (i - 1) : LagIntervalInSeconds : SegmentLengthInSeconds * i);
+                [~, ACF(i - StartFromThisSegment + 1, :)] = ...
+                    calculateDiscreteTimeSeriesStatistics(TimeSeries, ...
+                    ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
+                    floor(MaxLagInSeconds / LagIntervalInSeconds));
+            end
         end
-    else
-        % Time-tagged (time-resolved) mode - Trace.Data represent macro
-        % times of detected photons
-        Trace.ArrivalTime = cumsum(Trace.Data) / FCSHeader.ClockFrequency;
-        for i = StartFromThisSegment : SegmentsNumber
-            TimeSeries = histcounts(Trace.ArrivalTime, ...
-                SegmentLengthInSeconds * (i - 1) : LagIntervalInSeconds : SegmentLengthInSeconds * i);
-            [~, ACF(i - StartFromThisSegment + 1, :)] = ...
-                calculateDiscreteTimeSeriesStatistics(TimeSeries, ...
-                ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
-                floor(MaxLagInSeconds / LagIntervalInSeconds));
-        end
+        Trace{j}.ACF = mean(ACF);
+        Trace{j}.LagInSeconds = (ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
+            floor(MaxLagInSeconds / LagIntervalInSeconds)) * ...
+            LagIntervalInSeconds;
+        Trace{j}.SampleTime = FCSHeader.SampleTime;
     end
-    FCSAC = mean(ACF);
-    LagInSeconds = (ceil(MinLagInSeconds / LagIntervalInSeconds) : ...
-        floor(MaxLagInSeconds / LagIntervalInSeconds)) * ...
-        LagIntervalInSeconds;
 end
 
 function [TimeSeriesMean, NormalizedAutoCovariance] = ...
